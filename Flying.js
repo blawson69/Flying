8629f6cef85c2ef33ae6f689e5238df80d45c368
Ben L <39037350+blawson69@users.noreply.github.com>
2018-09-12 12:13:08 -0700

    2.0 release

---
+++ Flying.js
@@ -0,0 +1,81 @@
+﻿/*
+Flying
+A Roll20 script to indicate a flying token's number of feet above ground.
+
+Github:		https://github.com/blawson69/Flying
+Contact:	https://app.roll20.net/users/1781274/ben-l
+*/
+
+var Flying = Flying || (function() {
+    'use strict';
+
+    var version = '2.0',
+        flyMarker = 'fluffy-wing',
+
+    handleInput = function(msg) {
+		if (msg.type !== "api") {
+			return;
+		}
+
+        if (msg.content.startsWith('!fly')) {
+            if (!msg.selected) {
+                sendChat('Flying', '/w ' + msg.who + ' No tokens were selected!', null, {noarchive:true});
+                return;
+            }
+
+            var msgparts = msg.content.split(/\s+/), height = 0;
+            if (msgparts[1]) msgparts[1] = msgparts[1].replace(/[^\d]+/,'');
+            if (msgparts[1] && msgparts[1] !== 0) height = msgparts[1];
+
+            _.each(msg.selected, function(obj) {
+                var o = getObj(obj._type, obj._id);
+                if(o) {
+                    if (o.get("_type") === "graphic" ) {
+                        if (o.get("_subtype") === "token") {
+                            if (height == 0) {
+                                o.set('status_'+flyMarker, false);
+                            } else {
+                                var oM = o.get("statusmarkers"), nM = [];
+                                if (oM.length > 0) {
+                                    oM = oM.split(',');
+                                    _.each(oM, function(marker){
+                                        if (marker.includes(flyMarker) === false) nM.push(marker);
+                                    });
+                                }
+
+                                var nH = height.toString().split('').reverse();
+                                _.each(nH, function(num){
+                                    nM.push(flyMarker+'@'+num);
+                                });
+
+                                nM = nM.join(',');
+                                o.set({statusmarkers: nM});
+                            }
+                        }
+                    }
+                }
+            });
+
+        }
+    },
+
+	logReadiness = function (msg) {
+		log('--> Flying v' + version + ' <-- Initialized');
+	},
+
+    registerEventHandlers = function() {
+        on('chat:message', handleInput);
+    };
+
+    return {
+        logReadiness: logReadiness,
+        RegisterEventHandlers: registerEventHandlers
+    };
+
+}());
+
+on("ready",function(){
+    'use strict';
+    Flying.logReadiness();
+    Flying.RegisterEventHandlers();
+});
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Flying
+
+This  [Roll20](http://roll20.net/) script places one or more status icons on a selected token to indicate a number of feet above ground that the token is "flying."
+
+### Usage
+With a token selected, enter `!fly` followed by the number of feet above ground that the token is "flying." For each digit in that number, a fluffy-wing status marker will appear with a badge for the digit on the token. For instance, if you enter `!fly 45` the selected token will get two fluffy-wing markers, one with a 4 badge and one with a 5 badge. When the token "lands," you may pass a zero or nothing at all and the status marker(s) will be removed. Multiple tokens may be selected.
+
+### Customization
+
+You may use a status marker other than 'fluffy-wing' if you wish. Just choose one of the status marker names from the list in the [API documentation](https://wiki.roll20.net/API:Objects#Graphic_.28Token.2FMap.2FCard.2FEtc..29) and change the `flyMarker` variable at the top of the script to the name of desired marker.
