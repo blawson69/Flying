2415a60d7d2cf86f0763c141da75ea4330af0624
Ben L <39037350+blawson69@users.noreply.github.com>
2019-08-30 19:21:12 -0700

    Update Flying.js
    Corrected Venmo link in README

--- Flying.js
+++ Flying.js
@@ -4,7 +4,7 @@ A Roll20 script to indicate a flying token's number of feet above ground.
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theBenLawson
+Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
 */
 
 var Flying = Flying || (function() {
