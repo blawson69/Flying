4198f98df749bb1685a1ecb9732cc51446f0484d
Ben L <39037350+blawson69@users.noreply.github.com>
2020-01-15 12:33:50 -0800

    Update to 2.2
    Added support for custom token markers.

--- Flying.js
+++ Flying.js
@@ -4,27 +4,37 @@ A Roll20 script to indicate a flying token's number of feet above ground.
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
+
+Like this script? Become a patron:
+    https://www.patreon.com/benscripts
 */
 
 var Flying = Flying || (function() {
     'use strict';
 
-    var version = '2.1',
-        debugMode = false,
-        styles = {
-            box:  'background-color: #fff; border: 1px solid #000; padding: 8px 10px; border-radius: 6px; margin-left: -40px; margin-right: 0px;',
-            title: 'padding: 0 0 10px 0; color: ##591209; font-size: 1.5em; font-weight: bold; font-variant: small-caps; font-family: "Times New Roman",Times,serif;',
-            button: 'background-color: #000; border-width: 0px; border-radius: 5px; padding: 5px 8px; margin: 8px 0; color: #fff; text-align: center;',
-            textButton: 'background-color: transparent; border: none; padding: 0; color: #591209; text-decoration: underline;',
-            code: 'display: inline; font-family: "Courier New", Courier, monospace; background-color: #ddd; color: #000; padding: 1px 6px;',
-        },
+    var version = '2.2',
+    debugMode = false,
+    MARKERS,
+    ALT_MARKERS = [{name:'red', tag: 'red', url:"#C91010"}, {name: 'blue', tag: 'blue', url: "#1076C9"}, {name: 'green', tag: 'green', url: "#2FC910"}, {name: 'brown', tag: 'brown', url: "#C97310"}, {name: 'purple', tag: 'purple', url: "#9510C9"}, {name: 'pink', tag: 'pink', url: "#EB75E1"}, {name: 'yellow', tag: 'yellow', url: "#E5EB75"}, {name: 'dead', tag: 'dead', url: "X"}],
+    styles = {
+        box:  'background-color: #fff; border: 1px solid #000; padding: 8px 10px; border-radius: 6px; margin-left: -40px; margin-right: 0px;',
+        title: 'padding: 0 0 10px 0; color: ##591209; font-size: 1.5em; font-weight: bold; font-variant: small-caps; font-family: "Times New Roman",Times,serif;',
+        button: 'background-color: #000; border-width: 0px; border-radius: 5px; padding: 5px 8px; margin: 8px 0; color: #fff; text-align: center;',
+        buttonWrapper: 'text-align: center; margin: 10px 0 4px; clear: both;',
+        textButton: 'background-color: transparent; border: none; padding: 0; color: #591209; text-decoration: underline;',
+        code: 'display: inline; font-family: "Courier New", Courier, monospace; background-color: #ddd; color: #000; padding: 1px 6px;',
+    },
 
     checkInstall = function () {
         if (!_.has(state, 'Flying')) state['Flying'] = state['Flying'] || {};
         if (typeof state['Flying'].flyMarker == 'undefined') state['Flying'].flyMarker = 'fluffy-wing';
-        log('--> Flying v' + version + ' <-- Initialized');
-        if (debugMode) showDialog('Debug Mode', 'Flying has landed. Er, I mean loaded...');
+        var chatter = ['Ready for takeoff','Clear the runway','Fasten your seatbelts','Tray tables up','We may experience turbulance'];
+        MARKERS = JSON.parse(Campaign().get("token_markers"));
+        log('--> Flying v' + version + ' <-- ' + chatter[randomInteger(_.size(chatter)-1)] + '...');
+        if (debugMode) {
+			var d = new Date();
+			showDialog('Debug Mode', 'Flying v' + version + ' loaded at ' + d.toLocaleTimeString() + '<br><a style=\'' + styles.textButton + '\' href="!fly-config">Show config</a>', 'GM');
+		}
     },
 
     handleInput = function(msg) {
@@ -78,64 +88,90 @@ var Flying = Flying || (function() {
 
     showConfig = function () {
         var marker_style = 'margin: 5px 10px 0 0; display: block; float: left;';
-        var message = '<span style=\'' + styles.code + '\'>!fly &lt;number&gt;</span> sets the Flying Marker on the selected token(s) with a marker for every digit. '
-        + 'Sending <span style=\'' + styles.code + '\'>!fly 45</span> will place a Flying Marker with a "4" and a Flying Marker with a "5" on the token.<br><br>'
-        + 'A zero or a blank after <span style=\'' + styles.code + '\'>!fly</span> will "land" the token by removing all Flying Markers.<br><br>';
+        var message = '<span style=\'' + styles.code + '\'>!fly &lt;number&gt;</span> sets the Flying token marker on the selected token(s) with a marker for every digit. '
+        + 'Sending <span style=\'' + styles.code + '\'>!fly 45</span> will place one Flying marker with a "4" and another with a "5" on the token.<br><br>'
+        + 'No digits after <span style=\'' + styles.code + '\'>!fly</span> will "land" the token by removing all Flying markers.';
 
-        message += '<h4>Flying Marker</h4>' + getMarker(state['Flying'].flyMarker, marker_style)
-        + 'The current status marker to indicate a flying token is "' + state['Flying'].flyMarker + '".<br>';
-        message += '<div align="center"><a style="' + styles.button + '" href="!fly-markers">Change Marker</a></div>';
+        var curr_marker = _.find(MARKERS, function (x) { return x.tag == state['Flying'].flyMarker; });
+        if (typeof curr_marker == 'undefined') curr_marker = _.find(ALT_MARKERS, function (x) { return x.tag == state['Flying'].flyMarker; });
+        message += '<hr><h4>Flying Marker</h4>' + getMarker(curr_marker, marker_style) + 'This is the current Flying token marker. You may change it below.';
+        message += '<div style="' + styles.buttonWrapper + '"><a style="' + styles.button + '" href="!fly-markers" title="This may result in a very long list...">Choose Marker</a></div>';
+        message += '<div style="text-align: center;"><a style="' + styles.textButton + '" href="!fly-set &#63;&#123;Token Marker&#124;&#125;">Set manually</a></div>';
+
         showDialog('Help Menu', message);
     },
 
     showMarkers = function () {
-        var status_markers = ['blue', 'brown', 'green', 'pink', 'purple', 'yellow', 'fluffy-wing', 'angel-outfit'];
-        var message = 'Note: Tokens with Flying Markers on them already will <b>not</b> be updated with the new marker.<br><table style="border: 0; width: 100%;" cellpadding="0" cellspacing="2">';
-        _.each(status_markers, function(marker) {
-            message += '<tr><td>' + getMarker(marker, 'margin-right: 10px;') + '</td><td style="white-space: nowrap; width: 100%;">' + marker + '</td>';
+        var message = '<table style="border: 0; width: 100%;" cellpadding="0" cellspacing="2">';
+        _.each(ALT_MARKERS, function (marker) {
+            message += '<tr><td>' + getMarker(marker, 'margin-right: 10px;') + '</td><td style="white-space: nowrap; width: 100%;">' + marker.name + '</td>';
             if (marker == state['Flying'].flyMarker) {
                 message += '<td style="text-align: center;">Current</td>';
             } else {
-                message += '<td style="text-align: center; white-space: nowrap;"><a style="' + styles.button + '" href="!fly-set ' + marker + '">Set Marker</a></td>';
+                message += '<td style="text-align: center; white-space: nowrap;"><a style="' + styles.button + '" href="!fly-set ' + marker.tag + '">Set Marker</a></td>';
             }
             message += '</tr>';
         });
-        message += '<tr><td colspan="3" style="text-align: center;"><a style="' + styles.button + '" href="!fly-config">&#9668; Back</a> &nbsp; <a style="'
-        + styles.button + '" href="!fly-set &#63;&#123;Status Marker&#124;&#125;">Different Marker</a></td></tr>';
+
+        _.each(MARKERS, function (icon) {
+            message += '<tr><td>' + getMarker(icon, 'margin-right: 10px;') + '</td><td style="white-space: nowrap; width: 100%;">' + icon.name + '</td>';
+            if (icon.tag == state['Flying'].flyMarker) {
+                message += '<td style="text-align: center;">Current</td>';
+            } else {
+                message += '<td style="text-align: center; white-space: nowrap;"><a style="' + styles.button + '" href="!fly-set ' + icon.tag.replace('::','=') + '">Set Marker</a></td>';
+            }
+            message += '</tr>';
+        });
+
+        message += '<tr><td colspan="3" style="text-align: center;"><a style="' + styles.button + '" href="!fly-config">&#9668; Back</a></td></tr>';
         message += '</table>';
+
         showDialog('Choose Flying Marker', message);
     },
 
     setMarker = function (marker) {
+        /*
         var status_markers = ['blue', 'brown', 'green', 'pink', 'purple', 'red', 'yellow', 'all-for-one', 'angel-outfit', 'archery-target', 'arrowed', 'aura', 'back-pain', 'black-flag', 'bleeding-eye', 'bolt-shield', 'broken-heart', 'broken-shield', 'broken-skull', 'chained-heart', 'chemical-bolt', 'cobweb', 'dead', 'death-zone', 'drink-me', 'edge-crack', 'fishing-net', 'fist', 'fluffy-wing', 'flying-flag', 'frozen-orb', 'grab', 'grenade', 'half-haze', 'half-heart', 'interdiction', 'lightning-helix', 'ninja-mask', 'overdrive', 'padlock', 'pummeled', 'radioactive', 'rolling-bomb', 'screaming', 'sentry-gun', 'skull', 'sleepy', 'snail', 'spanner', 'stopwatch', 'strong', 'three-leaves', 'tread', 'trophy', 'white-tower'];
         if (_.find(status_markers, function (tmp) {return tmp === marker; })) {
             state['Flying'].flyMarker = marker;
         } else {
             showDialog('Error', 'The status marker "' + marker + '" is invalid. Please try again.');
         }
+        */
+
+        marker = marker.replace('=', '::');
+        var status_markers = _.pluck(MARKERS, 'tag');
+        _.each(_.pluck(ALT_MARKERS, 'tag'), function (x) { status_markers.push(x); });
+        if (_.find(status_markers, function (tmp) {return tmp === marker; })) {
+            state['Flying'].flyMarker = marker;
+        } else {
+            showDialog('Error', 'The token marker "' + marker + '" is invalid. Please try again.');
+        }
+
         showConfig();
     },
 
     getMarker = function (marker, style = '') {
-        let X = '';
-        let marker_style = 'width: 24px; height: 24px;';
-        var marker_pos = {red:"#C91010",  blue: "#1076C9",  green: "#2FC910",  brown: "#C97310",  purple: "#9510C9",  pink: "#EB75E1",  yellow: "#E5EB75",  dead: "X",  skull: 0, sleepy: 34, "half-heart": 68, "half-haze": 102, interdiction: 136, snail: 170, "lightning-helix": 204, spanner: 238, "chained-heart": 272, "chemical-bolt": 306, "death-zone": 340, "drink-me": 374, "edge-crack": 408, "ninja-mask": 442, stopwatch: 476, "fishing-net": 510, overdrive: 544, strong: 578, fist: 612, padlock: 646, "three-leaves": 680, "fluffy-wing": 714, pummeled: 748, tread: 782, arrowed: 816, aura: 850, "back-pain": 884, "black-flag": 918, "bleeding-eye": 952, "bolt-shield": 986, "broken-heart": 1020, cobweb: 1054, "broken-shield": 1088, "flying-flag": 1122, radioactive: 1156, trophy: 1190, "broken-skull": 1224, "frozen-orb": 1258, "rolling-bomb": 1292, "white-tower": 1326, grab: 1360, screaming: 1394,  grenade: 1428,  "sentry-gun": 1462,  "all-for-one": 1496,  "angel-outfit": 1530,  "archery-target": 1564};
+        var marker_style = 'width: 24px; height: 24px;' + style;
+        var return_marker = '<div style="' + marker_style + ' font-family: Webdings; color: #c00;">x</div>';
+        if (typeof marker != 'undefined' && typeof marker.tag != 'undefined') {
+            var status_markers = _.pluck(MARKERS, 'tag'),
+            alt_marker = _.find(ALT_MARKERS, function (x) { return x.tag == marker.tag; });
 
-        if (typeof marker_pos[marker] === 'undefined') return false;
-
-        if (Number.isInteger(marker_pos[marker])) {
-            marker_style += 'background-image: url(https://roll20.net/images/statussheet.png);'
-            + 'background-repeat: no-repeat; background-position: -' + marker_pos[marker] + 'px 0;';
-        } else if (marker_pos[marker] === 'X') {
-            marker_style += 'color: #C91010; font-size: 32px; font-weight: bold; text-align: center; padding-top: 5px; overflow: hidden;';
-            X = 'X';
-        } else {
-            marker_style += 'background-color: ' + marker_pos[marker] + '; border: 1px solid #fff; border-radius: 50%;';
+            if (_.find(status_markers, function (x) { return x == marker.tag; })) {
+                var icon = _.find(MARKERS, function (x) { return x.tag == marker.tag; });
+                return_marker = '<img src="' + icon.url + '" width="24" height="24" style="' + marker_style + '" />';
+            } else if (typeof alt_marker !== 'undefined') {
+                if (alt_marker.url === 'X') {
+                    marker_style += 'color: #C91010; font-size: 30px; line-height: 24px; font-weight: bold; text-align: center; padding-top: 0px; overflow: hidden;';
+                    return_marker = '<div style="' + marker_style + '">X</div>';
+                } else {
+                    marker_style += 'background-color: ' + alt_marker.url + '; border: 1px solid #fff; border-radius: 50%;';
+                    return_marker = '<div style="' + marker_style + '"></div>';
+                }
+            }
         }
-
-        marker_style += style;
-
-        return '<div style="' + marker_style + '">' + X + '</div>';
+        return return_marker;
     },
 
     showDialog = function (title, content, whom = 'GM') {
--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Flying
 
-This  [Roll20](http://roll20.net/) script places one or more status icons on a selected token to indicate a number of feet above ground that the token is "flying."
+> **New in v2.2:** TokenLocker has been updated to support the new [custom token markers](https://wiki.roll20.net/Custom_Token_Markers).
+
+This  [Roll20](http://roll20.net/) script places one or more token markers on a selected token(s) to indicate a number of feet above ground that the token is "flying."
 
 ## Usage
-With a token selected, enter `!fly` followed by the number of feet above ground that the token is "flying." For each digit in that number, a Flying Marker will appear with a badge for the digit on the token. For instance, if you enter `!fly 45` the selected token will get two Flying Markers, one with a 4 badge and one with a 5 badge. When the token "lands," you may pass a zero or nothing at all (just `!fly`) and the Flying Marker(s) will be removed. Multiple tokens may be selected.
+With one or more tokens selected, enter `!fly` followed by the number of feet above ground that the token is "flying." For each digit in that number, a Flying Marker will appear with a badge for the digit on the token. For instance, if you enter `!fly 45` the selected token will get two Flying Markers, one with a 4 badge and one with a 5 badge. When the token "lands," you may pass a zero or nothing at all (just `!fly`) and the Flying Marker(s) will be removed.
 
 ## Configuration
 
-`!fly-config` will display a Config Menu that give you the commands in the chat window.
+`!fly-config` will display a Config Menu that give you the commands in the chat window and the currently set Flying Marker.
 
 ## Flying Marker
 
-The default status marker for Flying is the "fluffy-wing" marker, but you may use another if you wish. Clicking the "Change Marker" button in the Config Menu will give a short list of suggested status markers to use and gives the option to input your own from the currently valid status markers.
+The Flying Marker is the token marker used to indicate Flying, and defaults to the "fluffy-wing" status marker. The GM can change the Flying Marker to any token marker desired. The Config Menu provides a "Choose Marker" button to display all token markers *including custom token markers* for easy selection, or you can use the "set manually" link to provide the name or name::ID combo for any valid token markers.
+
+Note that because Flying updates status markers automatically, there could be conflict with scripts such as [EncumbranceTracker](https://github.com/blawson69/EncumbranceTracker), [ExhaustionTracker](https://github.com/blawson69/ExhaustionTracker), [CombatTracker](https://github.com/vicberg/Combattracker), and others and others that also modify token markers. If changing the Flying Marker, **make sure you choose a token marker that is not being used by another script.**
